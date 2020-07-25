0be534de2f58684fd8c9f518baa20f2f86a32fdb
DESKTOP-D18M4CI\ZTT <zss93157623@qq.com>
2020-07-25 08:27:15 +0800

    Initial commit

---
+++ src/test/java/jasper/print/printTest.java
@@ -0,0 +1,37 @@
+package jasper.print;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import org.exolab.castor.dsml.XML;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class printTest {
+
+    private static final String path= "F:/JasperPrint/";
+    private static final String jasperPath= "jasper/";
+    private static final String pdfPath= "pdf/";
+
+    @Test
+    public void printJasper(){
+        String jasperName = "test.jasper";
+        String pdfName = "test.pdf";
+        Map<String,Object> params = new HashMap<String, Object>();
+        params.put("Parameter1","18068161321");
+        params.put("Parameter2","1225631395");
+        params.put("Parameter3","1466027294");
+        params.put("Parameter4","wikipedia@feiliki.com");
+        try {
+            JRDataSource dataSource =new JREmptyDataSource();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(path+jasperPath+jasperName,params,dataSource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint,path+pdfPath+pdfName);
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
+    }
+}
